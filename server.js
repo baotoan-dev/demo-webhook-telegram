fbe0de38a9cd513ed0279643f7c051c0a726c8d2
Huynh Bao Toan <toanhuynh03995@S3P.COM>
2025-08-19 09:55:18 +0700

    feat: remove text server from log

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Node.js server test!");
+  res.end("Hello from Node.js server!");
 });
 
 server.listen(PORT, () => {
