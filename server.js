ee31c4c065ba89541bb9c4d4b959d48ac653681b
Huynh Bao Toan <toanhuynh03995@S3P.COM>
2025-08-19 09:52:54 +0700

    feat: update log server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Node.js server!');
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Node.js server test!");
 });
 
 server.listen(PORT, () => {
